41502288e1b8a4201288ca4738d356dea80f89cc
Ferrp86 <freviriegop86@gmail.com>
2021-12-13 17:03:23 +0100

    Registro de usuario

---
+++ orbita/models/usuario.model.js
@@ -0,0 +1,11 @@
+const { executeQuery } = require('../utils');
+
+const createUser = ({ nombre, primer_apellido, segundo_apellido, email, fecha_nacimiento, nombre_usuario, password }) => {
+    return executeQuery('insert into usuarios (nombre, primer_apellido,segundo_apellido,email, fecha_nacimiento, nombre_usuario, password) values (?,?,?,?,?,?,?)', [nombre, primer_apellido, segundo_apellido, email, fecha_nacimiento, nombre_usuario, password]);
+}
+
+const getUser = ({ email, password }) => {
+    return executeQueryOne('select * from usuarios where email = ? and password = ?', [email, password]);
+}
+
+module.exports = { createUser, getUser }
