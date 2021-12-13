69839535df010a497f3036017d8e603d784e3e5c
Ferrp86 <freviriegop86@gmail.com>
2021-12-13 23:25:51 +0100

    Login de usuario con mensaje de error en caso de datos incorrectos

--- orbita/routes/api/usuarios.js
+++ orbita/routes/api/usuarios.js
@@ -1,21 +1,26 @@
 const router = require('express').Router();
-const { createUser } = require('../../models/usuario.model');
+const { createUser, getUser } = require('../../models/usuario.model');
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    let result;
     try {
-        res.json({ msg: 'hellou' })
+        result = await getUser(req.body);
     } catch (err) {
         res.json({ error: err.message });
     }
+
+    if (!result) {
+        return res.json({ Error: 'Email o password incorrectos' });
+    }
+    res.json(result);
 });
 
 router.post('/registro', async (req, res) => {
-
     try {
         const result = await createUser(req.body);
         res.json(result);
     } catch (err) {
-        console.log(err.message);
+        res.json({ error: err.message });
     }
 });
 
--- orbita/utils.js
+++ orbita/utils.js
@@ -7,4 +7,14 @@ const executeQuery = (query, data = []) => {
     });
 }
 
-module.exports = { executeQuery }
+const executeQueryOne = (query, data = []) => {
+    return new Promise((resolve, reject) => {
+        db.query(query, data, (err, result) => {
+            if (err) return reject(err);
+            if (result.length === 0) return resolve(null);
+            resolve(result[0]);
+        })
+    });
+}
+
+module.exports = { executeQuery, executeQueryOne }
