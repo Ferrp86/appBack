07ffbbc3dfcbc7db3af54387a2c522a912e73f76
leost28 <leost28@gmail.com>
2021-12-16 21:40:03 +0100

    ruteo evento

--- orbita/models/evento.model.js
+++ orbita/models/evento.model.js
@@ -1,7 +1,7 @@
 const { executeQuery } = require('../utils');
 
 const createEvent = ({ nombre_evento, descripcion, localizacion, fecha_evento, categoria }) => {
-    return executeQuery('insert into eventos ( nombre_evento, descripcion, localizacion, fecha_evento, categoria) values (?,?,?,?,?,?,?)', [nombre_evento, descripcion, localizacion, fecha_evento, categoria]);
+    return executeQuery('insert into eventos ( nombre_evento, descripcion, localizacion, fecha_evento, categoria) values (?,?,?,?,?)', [nombre_evento, descripcion, localizacion, fecha_evento, categoria]);
 };
 
 module.exports = { createEvent };
